4387277fb687af563c713919d1a3be48236cbb0d
A-Tetarwal <andashishwill@gmail.com>
2024-06-25 12:24:48 +0530

    New Files

---
+++ basicsPython.py
@@ -0,0 +1,34 @@
+print("Hello python, World")
+print('bae bae')
+
+# variables
+naamHai = "Ashish"
+umrBtao = 20
+
+print('Naam:' + naamHai + ' \nAayu: ', umrBtao)
+sahiBtaya = True
+
+
+# practice1
+firstName = "Tony"
+lastName = "Stark"
+tonyAge = 51
+is_Genius = True
+
+
+# user se input lena
+# name = input("What is your name?\n")
+# print("Hello " + name)
+
+# type conversion -> isliye zaruri hota hai kyon ki jo bhi input string int float etc lete hain user se vo sab hmare
+# paas aata hai string format mein
+# old_age = input("Enter your age 2yrs ago: ")
+# new_age = int(old_age) + 2
+# print("current age: ", new_age)
+
+
+# sum of two nunmbers
+fst = input()
+scnd = input()
+
+print(int(fst) + int(scnd))
